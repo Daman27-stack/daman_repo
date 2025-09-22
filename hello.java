6211383940148c84838a83c5fa7a188049155ece
Daman27-stack <Kaurdamandeep815@gmail.com>
2025-09-22 11:41:02 +0530

    Update hello.java Simple SOP

--- hello.java
+++ hello.java
@@ -1,20 +1,14 @@
 // Simple Hello and Input Program
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        
 
         // Print Hello
         System.out.println("Hello, Welcome to Java!");
 
-        // Take input
-        System.out.print("Enter your name: ");
-        String name = sc.nextLine();
-
-        // Print message
-        System.out.println("Nice to meet you, " + name + "!");
-
-        sc.close();
+     
     }
 }
+
